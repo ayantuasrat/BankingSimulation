9ae398b330cd816adc6253f9dbefc484b7793375
Nathaniel Ketema <nathanielketema@gmail.com>
2025-04-29 14:48:13 -0500

    refactor: Refactor BankAccount into abstract Account base class with abstract methods for operations

--- BankAccount.cs
+++ BankAccount.cs
@@ -6,25 +6,27 @@ using System.Threading.Tasks;
 
 namespace BankingSimulation
 {
-    public class BankAccount
+    public abstract class Account
     {
         public string Name { get; set; }
-        public string AccountType { get; set; }
         public decimal Balance { get; set; }
         public string AccountNumber { get; set; }
 
-        public BankAccount(string name, string accountType, decimal initialDeposit)
+        public Account(string name, decimal initialDeposit)
         {
             Name = name;
-            AccountType = accountType;
             Balance = initialDeposit;
             AccountNumber = GenerateAccountNumber();
         }
 
-        private string GenerateAccountNumber() 
+        private string GenerateAccountNumber()
         {
             Random rnd = new Random();
-            return "AC" + rnd.Next(100000,999999).ToString();
+            return "AC" + rnd.Next(100000, 999999).ToString();
         }
+
+        public abstract void Deposit(decimal amount);
+        public abstract bool Withdraw(decimal amount);
+        public abstract bool Transfer(Account toAccount, decimal amount);
     }
 }
