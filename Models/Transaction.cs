51c4275c8309b9bb1471b0d1dfab6aaf6e27b330
Nathaniel Ketema <nathanielketema@gmail.com>
2025-04-29 15:05:49 -0500

    chore: Organize model classes into Models folder

---
+++ Models/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSimulation
+{
+    public class Transaction
+    {
+        public string AccountNumber {  get; set; }
+        public string Type {  get; set; }
+        public decimal Amount {  get; set; }
+        public DateTime Date { get; set; }
+        public string Note {  get; set; }
+        
+        public Transaction(string accountNumber,string type,decimal amount, string note = "")
+        {
+            AccountNumber = accountNumber;
+            Type = type;
+            Amount = amount;
+            Date = DateTime.Now;
+            Note = note;
+        }
+
+    }
+}
